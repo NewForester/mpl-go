89c3d2b39a46e2272aacc97c54a9f64900167e01
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-04-26 08:17:00 +0100

    revise solution for the Exercism prime-factors exercise

--- exercism/prime_factors.go
+++ exercism/prime_factors.go
@@ -1,67 +1,40 @@
 // Package prime provides a Go solution for the Exercism prime factors slug.
 package prime
 
-import "math"
-
 // Factors takes a natural number and returns its prime factors in ascending order
 func Factors(number int64) (factors []int64) {
 	factors = []int64{}
 
-	primes := []int64{2, 3, 5, 7, 11}
-	prime := int64(1)
+	for ; number&1 == 0; number >>= 1 {
+		factors = append(factors, 2)
+	}
 
-	for number > 1 {
-		prime, primes = nextPrime(prime, primes)
-
-		for number%prime == 0 {
-			factors = append(factors, prime)
-
-			number = number / prime
+	for candy := int64(3); number > 1; candy += 2 {
+		for ; number%candy == 0; number /= candy {
+			factors = append(factors, candy)
 		}
 	}
 
 	return
 }
 
-/// nextPrime() returns the next prime, extending the list of primes as necessary
-func nextPrime(prime int64, primes []int64) (int64, []int64) {
-	var candidate int64 = 1
-
-	for _, candidate = range primes {
-		if candidate > prime {
-			return candidate, primes
-		}
-	}
-
-	for isprime := false; !isprime; {
-		candidate = candidate + 1
-
-		limit := int64(math.Sqrt(float64(candidate)))
-
-		for _, prime := range primes {
-			if prime > limit {
-				isprime = true
-				break
-			}
-			if candidate%prime == 0 {
-				break
-			}
-		}
-	}
-
-	primes = append(primes, candidate)
-
-	return candidate, primes
-}
-
 //
-// My solution incorporates code from the nth-prime exercise and involves the
-// construction of a list of primes.
+// This second solution does not incorporate any code from the nth-prime
+// exercise and does not involve the generation of a list of primes.
 //
-// Only one other solution did this but the list of primes was generated in
-// parallel.  This solution was a order of magnitude faster than mine but I
-// am not sure that this has anything to do with the parallelism.
+// It does as almost all the other solutions did and uses a brute force
+// method of calculating factors.
 //
-// All the other solutions did not generate a list of primes.  They are much
-// simpler and I expect much faster.
+// It is a lot (orders of magitude) faster than my first attempt and this
+// appears to be because it cuts out the generation of the list of primes
+// and the memory managment / garbage collection that involves.  Beware.
+//
+// $ go test --bench .
+// goos: linux
+// goarch: amd64
+// BenchmarkPrimeFactors-2              100          16249759 ns/op
+// PASS
+// ok      _/home/work/exercism/go/prime-factors   1.663s
+//
+// which I think means the test suite takes 16 ms to run on my machine.
 //
